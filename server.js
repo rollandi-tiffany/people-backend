d10783f367d1f9323cd97500a160f115c7d42ff0
Tiffany Torres <torresrtiffany@gmail.com>
2023-09-16 09:56:14 -0700

    add cors and express.json

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const peopleController = require("./controllers/peopleController");
 const morgan = require("morgan");
+const cors = require("cors")
 
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 
 app.get("/", (req, res)=>{
@@ -17,4 +20,4 @@ app.use("/people", peopleController);
 
 
 
-app.listen(PORT , () => console.log("give me the love on port", PORT))
+app.listen(PORT , () => console.log("give me the love on port", PORT));
